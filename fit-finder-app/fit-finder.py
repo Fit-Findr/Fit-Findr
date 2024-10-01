66ba9214ee31f802d840d3267d044d01e5ce86d2
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2024-09-30 19:50:34 -0500

    add api testing

--- fit-finder-app/fit-finder.py
+++ fit-finder-app/fit-finder.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    point_url = "https://api.weather.gov/points/44.97997%2C-93.26384"
+    point_response = requests.get(point_url)
+    forecast_url = point_response.json()["properties"]["forecast"]
+    forecast_response = requests.get(forecast_url)
+    return forecast_response.json()
