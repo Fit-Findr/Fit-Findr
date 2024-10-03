ffd1464fa1b104509df8113a32bf96679fdde719
Krosauer <kyle.rosauer@icloud.com>
2024-10-03 10:34:48 -0500

    setup backend for sending images to. can recieve info rn but planning on working on image upload later on 10/3

---
+++ front-end/src/AddFits.jsx
@@ -0,0 +1,11 @@
+
+function AddFits() {
+    return (
+        <div>
+            <h1>Add Fits</h1>
+            
+        </div>
+    );
+}
+
+export default AddFits;
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,38 @@
-import ClothSelector from './ClothSelector/ClothSelector.jsx'
-import Navbar from './Navbar/Navbar.jsx'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import ClothSelector from './ClothSelector/ClothSelector.jsx';
+import AddFits from './AddFits.jsx'; // Import your AddFits component
+import axios from 'axios';
 
 function App() {
+
+  // const fetchAPI = async () => {
+  //   const response = await axios.get('http://localhost:8080/api/users');
+  //   console.log(response.data.users);
+  // };
+
+  // useEffect(() => {
+  //   fetchAPI();
+  // }, []);
   
+
   return (
-      <>
-      <Navbar />
+    <>
+      <div className="navbar">
+        <h1>Fit Finder</h1>
+        <ul>
+          <Link to="/add-fits" className='routeLink'>
+          <li>Add Fits</li>
+          </Link>
+          <li>Sign in</li>
+        </ul>
+      </div>
       <div className='container'>
         <ClothSelector />
         <ClothSelector />
         <ClothSelector />
       </div>
-      </>
+    </>
   );
 }
 
