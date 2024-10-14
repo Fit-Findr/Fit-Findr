3139924d3041a52070bd27b1849a5c1a9ecf9b24
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2024-10-14 13:42:13 -0500

    Add weather API call

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     const [tops, setTops] = useState([]);
     const [bottoms, setBottoms] = useState([]);
     const [layers, setLayers] = useState([]);
+    const [temp, setTemp] = useState([]);
 
     useEffect(() => {
         // Fetch tops
@@ -40,6 +41,49 @@ function App() {
                 console.error('Error fetching layers:', error);
             }
         };
+
+        // Fetch weather data
+        const getTemperatureAtPoint = async (lat, lon) => { 
+            // Get data about lat lon point       
+            const point_url = "https://api.weather.gov/points/" + lat + "," + lon;
+            const response = await fetch(point_url);
+            if (!response.ok) {
+                console.log("Error fetching point data");
+                return;
+            }
+
+            // Get url for forecast at point
+            const point_json = await response.json();
+            const forecast_url = point_json["properties"]["forecast"];
+
+            // Fetch forecast data using url
+            const forecast_response = await fetch(forecast_url);
+            if (!forecast_response.ok) {
+                console.log("Error fetching forecast data");
+                return;
+            }
+
+            // Get temperature from the next available forecast period
+            const forecast_json = await forecast_response.json();
+            const forecast_periods = forecast_json["properties"]["periods"];
+            if (forecast_periods.length == 0) {
+                console.log("No available forecasts");
+                return;
+            }
+            const first_forecast_period = forecast_periods[0];
+            const temperature = first_forecast_period["temperature"];
+            setTemp(temperature);
+        }
+
+        const getWeatherForLocation = async () => {
+            // Get lat lon and fetch temp at that location
+            navigator.geolocation.getCurrentPosition((position) => {
+                getTemperatureAtPoint(position.coords.latitude, position.coords.longitude);
+              });
+        }
+
+
+        getWeatherForLocation();        
 
         // Call fetch functions
         fetchTops();
