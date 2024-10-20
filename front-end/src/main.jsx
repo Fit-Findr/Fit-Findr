7c1136331870f95e123a5e3d3cb6ca2da3261164
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2024-10-20 15:50:34 -0500

    change baseUrl

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import MainRouting from './MainRouting.jsx'
 import './index.css'
 
-export const baseUrl = `http://localhost:8080`;
+export const baseUrl = `fit-finder-app.azurewebsites.net:8080`;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
